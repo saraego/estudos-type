81b10bd4b1932f44a31e3c684fa9979ec7fc74b4
saraego <saraegofelixbarros@gmail.com>
2024-02-04 15:39:15 -0400

    Error-Handing at express

---
+++ src/errors/app.error.ts
@@ -0,0 +1,9 @@
+import {StatusCodes} from "http-status-codes"
+
+export class AppError extends Error {
+    public statusCode: StatusCodes
+    constructor(message:string,statusCode:StatusCodes){
+        super(message)
+        this.statusCode = statusCode
+    }
+}
--- src/serve.ts
+++ src/serve.ts
@@ -2,12 +2,14 @@ import express from "express"
 import { routeMain } from "./routes"
 import "dotenv/config"
 import { setupMongo } from "./database"
+import { errorHandle } from "./middlewares/error-handler.middleware"
 
 setupMongo().then(()=>{
     const app = express()
     const PORT = 3001
     app.use(express.json())
     app.use(routeMain)
+    app.use(errorHandle)
     app.listen(PORT,()=>{
         console.log(`App is running at http://localhost:${PORT}`);
         
